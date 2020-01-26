1cf894001283611d6c936a38bba152dccb2eb7fa
Thyago Volpatto <tvolpatto@gmail.com>
2020-01-26 11:42:46 -0700

    Removed Carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,16 @@
 
 (function() {
-    bulmaCarousel.attach('#projects-carousel', {
-        slidesToScroll: 1,
-        slidesToShow: 3,
-        infinite: true
-      });
-
-    // The following code is based off a toggle menu by @Bradcomp
+        // The following code is based off a toggle menu by @Bradcomp
     // source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
     var burger = document.querySelector('.burger');
     var menu = document.querySelector('#'+burger.dataset.target);
     burger.addEventListener('click', function() {
         burger.classList.toggle('is-active');
         menu.classList.toggle('is-active');
-    });
+    });   
     
 })();
+
+function toggleColor(){
+    document.getElementsByClassName("is-changeable");
+}
